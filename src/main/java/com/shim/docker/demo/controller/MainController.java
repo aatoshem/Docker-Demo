bc700513ca8f387cbbdabfdf808fba2510c61d3d
aatoshem <atogheb@gmail.com>
2020-01-23 21:18:47 -0500

    Dummy Web App for demostrating containerizing with Docker

---
+++ src/main/java/com/shim/docker/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.shim.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+	public String getHello() {
+		return "<h1><b>Hello to the Docker Demo!</b></h1>";
+	}
+}
